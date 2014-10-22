ee37137f5bede6dfa1d4c4dcc5eaf6318c3f224c
Insane <luojieyy@gmail.com>
2014-10-22 15:09:56 +0800

    finish initialize

--- app/plugins/demo/index.coffee
+++ app/plugins/demo/index.coffee
@@ -1,16 +1,15 @@
 define('demo', ()->
 	user_plugin = 	
 		name : 'demo'
-		_models : {}
 		anchor : '#/demo'
 		title : 'Demo'
 		type : 'plugin'
-		order : -12
 		icon : 'icon-list'
 		# initialize plugin,
 		init : ()->
 			console.log 'init'
 			define_controller()
+			foundry.initialized(this.name)
 )
 
 define_controller = ()->
--- app/plugins/demo/index.js
+++ app/plugins/demo/index.js
@@ -6,15 +6,14 @@
     var user_plugin;
     return user_plugin = {
       name: 'demo',
-      _models: {},
       anchor: '#/demo',
       title: 'Demo',
       type: 'plugin',
-      order: -12,
       icon: 'icon-list',
       init: function() {
         console.log('init');
-        return define_controller();
+        define_controller();
+        return foundry.initialized(this.name);
       }
     };
   });
