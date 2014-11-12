3fdd9028a1df0979f2d15da8bb5eeac37bf20bf0
Insane <luojieyy@gmail.com>
2014-11-12 16:31:43 +0800

    remove the auth check

--- app/app.js
+++ app/app.js
@@ -32,9 +32,7 @@
   });
 
   Nimbus.Auth.authorized_callback = function() {
-    if (Nimbus.Auth.authorized()) {
-      return $("#login_buttons").addClass("redirect");
-    }
+    $("#login_buttons").addClass("redirect");
   };
 
   foundry.ready(function() {
